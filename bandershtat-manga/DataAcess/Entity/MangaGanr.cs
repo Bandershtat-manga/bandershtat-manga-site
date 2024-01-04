fd2be65d454648b7f90263208bdf2d4af739241c
Vlad <115177420+MelnikVladyslav@users.noreply.github.com>
2024-01-04 19:37:12 +0200

    Add data

---
+++ bandershtat-manga/DataAcess/Entity/MangaGanr.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcess.Entity
+{
+    public class MangaGanr
+    {
+        public int MangaId { get; set; }
+        public int GanrId { get; set; }
+
+        public virtual Manga Manga { get; set; }
+        public virtual Ganr Ganr { get; set; }
+    }
+}
---
+++ bandershtat-manga/DataAcess/Entity/SaveManga.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcess.Entity
+{
+    public class SaveManga
+    {
+        public Manga Manga { get; set;}
+        public string SaveFileName { get; set; }
+        public ICollection<User>? Users { get; set; }
+    }
+}
